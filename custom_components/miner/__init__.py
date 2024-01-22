49e0a16686c0b8dc6ec95040f4d3d38ddc29e267
b-rowan <brett.rowan@icloud.com>
2024-01-22 11:47:58 -0700

    Add ConfigEntryNotReady to async_setup_entry.

--- custom_components/miner/__init__.py
+++ custom_components/miner/__init__.py
@@ -1,11 +1,13 @@
 """The Miner integration."""
 from __future__ import annotations
 
+import pyasic
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
 
-from .const import DOMAIN
+from .const import DOMAIN, CONF_IP
 from .coordinator import MinerCoordinator
 
 PLATFORMS: list[Platform] = [Platform.SENSOR, Platform.SWITCH, Platform.NUMBER]
@@ -13,6 +15,12 @@ PLATFORMS: list[Platform] = [Platform.SENSOR, Platform.SWITCH, Platform.NUMBER]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Miner from a config entry."""
+
+    miner_ip = entry.data[CONF_IP]
+    miner = await pyasic.get_miner(miner_ip)
+
+    if miner is None:
+        raise ConfigEntryNotReady("Miner could not be found.")
 
     m_coordinator = MinerCoordinator(hass, entry)
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = m_coordinator
