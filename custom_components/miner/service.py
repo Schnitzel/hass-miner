89eb1b05a2b13465068a44fedcb00d3b5ef268da
Brett Rowan <121075405+b-rowan@users.noreply.github.com>
2024-06-21 10:48:21 -0600

    Improve services handler.

--- custom_components/miner/service.py
+++ custom_components/miner/service.py
@@ -18,18 +18,28 @@ LOGGER = logging.getLogger(__name__)
 async def async_setup_services(hass: HomeAssistant) -> None:
     """Service handler setup."""
 
-    async def reboot(call: ServiceCall) -> None:
+    async def get_miner(call: ServiceCall):
         ip = call.data.get(CONF_IP)
+        miners = hass.data[DOMAIN]
 
-        miner = await pyasic.get_miner(ip)
-        await miner.reboot()
+        if ip:
+            return await pyasic.get_miner(ip)
+        else:
+            LOGGER.error(
+                f"Cannot reboot, must specify an IP from [{miners}]",
+            )
+
+
+    async def reboot(call: ServiceCall) -> None:
+        miner = await get_miner(call)
+        if miner is not None:
+            await miner.reboot()
 
     hass.services.async_register(DOMAIN, SERVICE_REBOOT, reboot)
 
     async def restart_backend(call: ServiceCall) -> None:
-        ip = call.data.get(CONF_IP)
-
-        miner = await pyasic.get_miner(ip)
-        await miner.restart_backend()
+        miner = await get_miner(call)
+        if miner is not None:
+            await miner.restart_backend()
 
     hass.services.async_register(DOMAIN, SERVICE_RESTART_BACKEND, restart_backend)
